ea42f88f23e7cf3377e8337e9bc6dec9b68c0c73
Jakob Leibetseder <leibetsederjakob@gmail.com>
2022-08-30 17:33:37 +0200

    removed install method for plugin use, removed old splitted tsconfig files

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,12 +1,5 @@
-import {DayCalendar, MonthCalendar, MonthCalendarDays, YearCalendar} from'./components'
-
-const plugin = {
-  install (Vue: {component: (name: string, component: any)=>void}) {
-    Vue.component('DayCalendar', DayCalendar);
-    Vue.component('MonthCalendar', MonthCalendar);
-    Vue.component('MonthCalendarDays', MonthCalendarDays);
-    Vue.component('YearCalendar', YearCalendar);
-  }
-}
-
-export default plugin
+import DayCalendar from './DayCalendar/DayCalendar.vue';
+import MonthCalendar from './MonthCalendar/MonthCalendar.vue';
+import MonthCalendarDays from './MonthCalendar/MonthCalendarDays.vue';
+import YearCalendar from './YearCalendar/YearCalendar.vue';
+export {DayCalendar, MonthCalendar, MonthCalendarDays, YearCalendar};
