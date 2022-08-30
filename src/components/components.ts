5312c6e9c8899da4776b15b765d0718f3bb4bd23
Jakob Leibetseder <leibetsederjakob@gmail.com>
2022-08-30 16:58:18 +0200

    rempoved default export in favor of named export

--- src/components/components.ts
+++ src/components/components.ts
@@ -1,5 +1,5 @@
-import * as dayCalendar from '@/components/DayCalendar';
-import * as monthCalendar from '@/components/MonthCalendar';
-import * as YearCalendar from '@/components/YearCalendar';
-
-export default {...dayCalendar, ...monthCalendar, ...YearCalendar};
+import DayCalendar from './DayCalendar/DayCalendar.vue';
+import MonthCalendar from './MonthCalendar/MonthCalendar.vue';
+import MonthCalendarDays from './MonthCalendar/MonthCalendarDays.vue';
+import YearCalendar from './YearCalendar/YearCalendar.vue';
+export {DayCalendar, MonthCalendar, MonthCalendarDays, YearCalendar};
